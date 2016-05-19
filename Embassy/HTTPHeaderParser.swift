8682c48d6c6e8194b1e596773940050771e749a1
Victor Lin <bornstub@gmail.com>
2016-05-19 16:47:09 -0700

    Make the HTTP parser works

--- Embassy/HTTPHeaderParser.swift
+++ Embassy/HTTPHeaderParser.swift
@@ -12,6 +12,7 @@ import Foundation
 struct HTTPHeaderParser {
     private static let CR = UInt8(13)
     private static let LF = UInt8(10)
+    private static let NEWLINE = (CR, LF)
     
     enum Element {
         case Head(method: String, path: String, version: String)
@@ -24,21 +25,48 @@ struct HTTPHeaderParser {
         case Headers
     }
     private var state: State = .Head
+    private var buffer: [UInt8] = []
     
     /// Feed data to HTTP parser
     ///  - Parameter data: the data to feed
     ///  - Returns: parsed headers elements
-    func feed(data: [UInt8]) -> [Element] {
-        
-        switch state {
-        case .Head:
+    mutating func feed(data: [UInt8]) -> [Element] {
+        buffer += data
+        var elements = [Element]()
+        while buffer.count > 0 {
+            // pair of (0th, 1st), (1st, 2nd), (2nd, 3rd) ... chars, so that we can find <LF><CR>
+            let charPairs: [(UInt8, UInt8)] = Array(zip(
+                buffer[0..<buffer.count - 1],
+                buffer[1..<buffer.count]
+            ))
+            // ensure we have <CR><LF> in current buffer
+            guard let index = (charPairs).indexOf({ $0 == HTTPHeaderParser.NEWLINE }) else {
+                // no <CR><LF> found, just return the current elements
+                return elements
+            }
+            let bytes = Array(buffer[0..<index])
+            let string = String(bytes: bytes, encoding: NSUTF8StringEncoding)!
+            buffer = Array(buffer[(index + 2)..<buffer.count])
             
-            break
-        case .Headers:
-            break
+            // TODO: the initial usage of this HTTP server is for iOS API server mocking only,
+            // we don't usually see malform requests, but if it's necessary, like if we want to put
+            // this server in real production, we should handle malform header then
+            switch state {
+            case .Head:
+                let string = String(bytes: bytes, encoding: NSUTF8StringEncoding)!
+                let parts = string.componentsSeparatedByString(" ")
+                elements.append(.Head(method: parts[0], path: parts[1], version: parts[2..<parts.count].joinWithSeparator(" ")))
+                state = .Headers
+            case .Headers:
+                // end of headers
+                guard bytes.count > 0 else {
+                    elements.append(.End(bodyPart: buffer))
+                    return elements
+                }
+                let parts = string.componentsSeparatedByString(": ")
+                elements.append(.Header(key: parts[0], value: parts[1..<parts.count].joinWithSeparator(": ")))
+            }
         }
-        
-        // TODO: parse the header here
-        return [.Head(method: "abc", path: "123", version: "1.0")]
+        return elements
     }
 }
--- EmbassyTests/HTTPHeaderParserTests.swift
+++ EmbassyTests/HTTPHeaderParserTests.swift
@@ -35,7 +35,7 @@ class HTTPHeaderParserTests: XCTestCase {
 
     func testSimpleParsing() {
         let header = "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\nbody goes here"
-        let parser = HTTPHeaderParser()
+        var parser = HTTPHeaderParser()
         let elements = parser.feed(Array(header.utf8))
         XCTAssertEqual(elements, [
             HTTPHeaderParser.Element.Head(method: "GET", path: "/index.html", version: "HTTP/1.1"),
@@ -45,17 +45,21 @@ class HTTPHeaderParserTests: XCTestCase {
     }
 
     func testPartialParsing() {
-        let line1Part1 = "GET /index"
+        let line1Part1 = "GET /index.html"
         let line1Part2 = " HTTP/1.1\r\n"
         
         let line2Part1 = "Host: www.exam"
-        let line2Part2 = "mple.com\r"
+        let line2Part2 = "ple.com\r"
         let line2Part3 = "\n"
         
         let line3Part1 = "\r"
         let line3Part2 = "\nhere comes the body"
         
-        let parser = HTTPHeaderParser()
+        var parser = HTTPHeaderParser()
+        
+        // try to feed empty array
+        XCTAssertEqual(parser.feed(Array()), [])
+        
         XCTAssertEqual(parser.feed(Array(line1Part1.utf8)), [])
         XCTAssertEqual(parser.feed(Array(line1Part2.utf8)), [
             HTTPHeaderParser.Element.Head(method: "GET", path: "/index.html", version: "HTTP/1.1")
