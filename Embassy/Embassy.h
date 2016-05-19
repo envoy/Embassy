9c455833328c2432dbda2a5639378542dc59ffbc
Victor Lin <bornstub@gmail.com>
2016-05-19 14:43:52 -0700

    Initial Commit

---
+++ Embassy/Embassy.h
@@ -0,0 +1,19 @@
+//
+//  Embassy.h
+//  Embassy
+//
+//  Created by Fang-Pen Lin on 5/19/16.
+//  Copyright © 2016 Fang-Pen Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Embassy.
+FOUNDATION_EXPORT double EmbassyVersionNumber;
+
+//! Project version string for Embassy.
+FOUNDATION_EXPORT const unsigned char EmbassyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Embassy/PublicHeader.h>
+
+
