f929c4e1eb3e3e7d4452a4ffc2f8a2ebdcef9c1a
Victor Lin <bornstub@gmail.com>
2016-06-01 12:47:27 -0700

    Use extension for MultiDictionary

--- Embassy/MultiDictionary.swift
+++ Embassy/MultiDictionary.swift
@@ -8,15 +8,43 @@
 
 import Foundation
 
-/// MultiDictionary is a Dictionary like container, but allow one key to have multiple values
-public struct MultiDictionary<Key: Hashable, Value>: CollectionType, ArrayLiteralConvertible {
+/// MultiDictionary is a Dictionary and Array like container, it allows one key to have multiple
+/// values
+public struct MultiDictionary<Key: Hashable, Value> {
     typealias ArrayType = Array<(Key, Value)>
     // Items in this multi dictionary
     private let items: ArrayType
     // Dictionary mapping from key to all values in order
     private let keyValuesMap: Dictionary<Key, Array<Value>>
 
-    // MARK: Indexable
+    // MARK: MultiDictionary
+    public init(items: Array<(Key, Value)>) {
+        self.items = items
+        var keyValuesMap: Dictionary<Key, Array<Value>> = [:]
+        for (key, value) in items {
+            var values = keyValuesMap[key] ?? []
+            values.append(value)
+            keyValuesMap[key] = values
+        }
+        self.keyValuesMap = keyValuesMap
+    }
+
+    /// Get all values for given key in occurrence order
+    ///  - Parameter key: the key
+    ///  - Returns: array of values for given key
+    public func valuesFor(key: Key) -> Array<Value>? {
+        return keyValuesMap[key]
+    }
+    /// Get the first value for given key if available
+    ///  - Parameter key: the key
+    ///  - Returns: first value for the key if available, otherwise nil will be returned
+    public subscript(key: Key) -> Value? {
+        return valuesFor(key)?.first
+    }
+}
+
+// MARK: Indexable
+extension MultiDictionary: Indexable {
     public typealias Index = ArrayType.Index
     public var startIndex: MultiDictionary.Index {
         return items.startIndex
@@ -24,7 +52,10 @@ public struct MultiDictionary<Key: Hashable, Value>: CollectionType, ArrayLitera
     public var endIndex: MultiDictionary.Index {
         return items.endIndex
     }
-    // MARK: CollectionType
+}
+
+// MARK: CollectionType
+extension MultiDictionary: CollectionType {
     public typealias Generator = ArrayType.Generator
     public typealias SubSequence = ArrayType.SubSequence
     public func generate() -> MultiDictionary.Generator {
@@ -54,7 +85,10 @@ public struct MultiDictionary<Key: Hashable, Value>: CollectionType, ArrayLitera
     public var first: MultiDictionary.Generator.Element? {
         return items.first
     }
-    // MARK: ArrayLiteralConvertible
+}
+
+// MARK: ArrayLiteralConvertible
+extension MultiDictionary: ArrayLiteralConvertible {
     public typealias Element = ArrayType.Element
     public init(arrayLiteral elements: MultiDictionary.Element...) {
         items = elements
@@ -65,31 +99,5 @@ public struct MultiDictionary<Key: Hashable, Value>: CollectionType, ArrayLitera
             keyValuesMap[key] = values
         }
         self.keyValuesMap = keyValuesMap
-    }
-
-    // MARK: MultiDictionary
-    public init(items: Array<(Key, Value)>) {
-        self.items = items
-        var keyValuesMap: Dictionary<Key, Array<Value>> = [:]
-        for (key, value) in items {
-            var values = keyValuesMap[key] ?? []
-            values.append(value)
-            keyValuesMap[key] = values
-        }
-        self.keyValuesMap = keyValuesMap
-    }
-
-    /// Get all values for given key in occurrence order
-    ///  - Parameter key: the key
-    ///  - Returns: array of values for given key
-    public func valuesFor(key: Key) -> Array<Value>? {
-        return keyValuesMap[key]
-    }
-
-    /// Get the first value for given key if available
-    ///  - Parameter key: the key
-    ///  - Returns: first value for the key if available, otherwise nil will be returned
-    public subscript(key: Key) -> Value? {
-        return valuesFor(key)?.first
     }
 }
