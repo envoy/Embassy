410ca9bc0312397941bb513d65f4ca5f82451a2e
Michael New <mikenew12@gmail.com>
2019-03-30 15:33:00 -0700

    Add products to swift package file

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Embassy",
+    products: [.library(name: "Embassy", targets: ["Embassy"])],
     targets: [.target(name: "Embassy", path: "./Sources")]
 )
