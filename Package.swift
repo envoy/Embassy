9e69ac1f6a440cba29a91de6fab14f3e342c39b2
Clement Prem <clementprem@gmail.com>
2019-10-15 17:41:42 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Embassy",
+     platforms: [
+        .iOS(.v11),
+    ],
     products: [.library(name: "Embassy", targets: ["Embassy"])],
     targets: [.target(name: "Embassy", path: "./Sources")]
 )
